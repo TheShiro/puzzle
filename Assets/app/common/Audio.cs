6a7109be8f8df3281a438a27ca5388e411c0b62c
TheShiro <smirnovsart@gmail.com>
2019-05-28 21:22:47 +0500

    end day

---
+++ Assets/app/common/Audio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Models;
+
+namespace Main {
+
+	public class Audio : MonoBehaviour {
+
+		public AudioSource audio;
+		public AudioClip[] clip;
+		public int i = 0;
+
+		void Start() {
+			AudioModel model = new AudioModel();
+			clip = model.GetList(100);
+		}
+		
+		void Update () {
+			if(!audio.isPlaying) {
+				audio.clip = clip[i];
+				audio.Play();
+				i++;
+				if(i > clip.Length) {
+					i = 0;
+				}
+			}
+		}
+	}
+
+}
---
+++ Assets/app/models/AudioModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Models {
+
+	public class AudioModel : ModelCore {
+
+		public string[] GetMusic(int ind) {
+			db.Select(new string[] {"name"});
+			db.From("audio");
+			db.Where(new string[,] { {"sector", ind + "", "text"} });
+
+			string[,] res = db.GetResult();
+
+			return null;
+		}
+
+		public AudioClip[] GetList(int ind) {
+
+			return null;
+		}
+	}
+
+}
